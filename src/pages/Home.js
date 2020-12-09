03f1748e37b2ad7aab5598e720e4d06ab97c12c7
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-09 23:48:38 +0530

    added cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,10 @@ import MainPageLayout from '../components/MainPageLayout'
 
 import {apiGet} from '../misc/config'
 
+import ShowGrid from '../components/show/ShowGrid'
+
+import ActorGrid from '../components/actor/ActorGrid'
+
 const Home = () => {
     const [input,setInput]=useState('');
     const [results,setResults]=useState(null);
@@ -36,12 +40,10 @@ const Home = () => {
            return <div>No results</div> 
         }
         if (results && results.length>0){
-            return results[0].show
-            ? results.map(item =><div key={item.show.id}>{item.show.name}</div>)
-            : results.map(item =>(
-                <div key={item.person.id}>{item.person.name}</div>
+            return results[0].show? <ShowGrid data={results}/>: <ActorGrid data={results}/>
+            
 
-            ))
+            
 
                 
 
