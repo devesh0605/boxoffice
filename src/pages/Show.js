72d6fd985e9e4339d9f9d6b348d6d44db337ea1f
devesh0605 <dsaxena_be18@thapar.edu>
2020-12-10 13:31:09 +0530

    added error messages

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,3 +1,4 @@
+
 import React, {useEffect,useState} from 'react'
 
 import {useParams} from 'react-router-dom'
@@ -6,20 +7,38 @@ import { apiGet } from '../misc/config';
 const Show = () =>{ 
     const {id}=useParams();
     const [show,setShow]=useState(null)
-
+    const [isLoading,setIsLoading]=useState(true)
+    const [error,setError]=useState(null)
     useEffect(()=>{
-        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
-            setShow(results)
+
+        let isMounted=true
+        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+        .then(results=>{
+            
+                if (isMounted){
+                setShow(results)
+                setIsLoading(false)}
+            
+           
+        }).catch(err=>{
+            setError(err.message)
+            setIsLoading(false)
         })
+        return ()=>{
+            isMounted=false
+        }
 
     },[id])
+    console.log(show)
     
     
-    return (
-        <div>
-            This is show page
-        </div>
-    )
+    if (isLoading){
+        return <div>Data is being loaded</div>
+    }
+    if (error){
+    return <div>Error occured: {error}</div>
+    }
+    return <div>This is show page</div>
 }
 
 export default Show
